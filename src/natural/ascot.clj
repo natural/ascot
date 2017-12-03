a77a3993b3f7172c6fe940eb8b9d2d879d6319d5
Troy Melhase <troy@troy.io>
2017-12-02 19:05:55 -0900

    Interns.

--- src/natural/ascot.clj
+++ src/natural/ascot.clj
@@ -1,3 +1,10 @@
 (ns natural.ascot
-  (:require [natural.ascot.core :refer []]))
+  (:require [natural.ascot.core]))
 
+
+(seq
+ (map (fn [[a b c]] (intern a b c))
+      [['natural.ascot 'column-specs natural.ascot.core/column-specs]
+       ['natural.ascot 'register     natural.ascot.core/register]
+       ['natural.ascot 'table-specs  natural.ascot.core/table-specs]
+       ['natural.ascot 'tables       natural.ascot.core/tables]]))
